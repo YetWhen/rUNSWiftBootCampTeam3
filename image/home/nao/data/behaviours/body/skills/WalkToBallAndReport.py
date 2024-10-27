f8717e0c6e5c545ff0dcfeae8e111b813cc1c33b
Diwen Ye <87795023+YetWhen@users.noreply.github.com>
2024-10-27 18:50:53 +1100

    Update WalkToBallAndReport.py with a potential fix of not entering Stand state
    The stand state maybe quickly passed by shifting to next state. This update is a potential fix and need to be tested.

--- image/home/nao/data/behaviours/body/skills/WalkToBallAndReport.py
+++ image/home/nao/data/behaviours/body/skills/WalkToBallAndReport.py
@@ -58,11 +58,11 @@ class WalkToBallAndReport(BehaviourTask):
             self._current_sub_task = "Stand"
             self._Stabled = True
             self._armTimer.start()
-        elif self._current_sub_task == "Stand" and self._armTimer.finished() and not self._ArmRaised:
+        elif self._current_sub_task == "Stand" and self._armTimer.finished() and self._Stabled not self._ArmRaised:
             self._current_sub_task = "Point"
             self._ArmRaised = True
             self._armTimer.start()
-        elif self._current_sub_task == "Point" and self._armTimer.finished():
+        elif self._current_sub_task == "Point" and self._armTimer.finished() and self._ArmRaised:
             self._current_sub_task = "CircleReport"
         
       
