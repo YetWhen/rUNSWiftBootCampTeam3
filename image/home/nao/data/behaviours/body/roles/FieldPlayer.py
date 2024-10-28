4dc0836a2c8820708fd1026d50debb563913d365
HaydenYe <hayden@L-DHY19Y3>
2024-10-28 15:05:53 +1100

    tried to roughly implement the Pass Ball to Teammate task

--- image/home/nao/data/behaviours/body/roles/FieldPlayer.py
+++ image/home/nao/data/behaviours/body/roles/FieldPlayer.py
@@ -2,7 +2,9 @@ import robot
 from importlib import import_module
 from util.Vector2D import Vector2D
 from BehaviourTask import BehaviourTask
+from util.TeamStatus import get_teammate_pos
 from body.skills.Stand import Stand
+from body.skills.P2PPass import P2PPass
 from util.Constants import FIELD_LENGTH, PENALTY_AREA_LENGTH, CENTER_CIRCLE_DIAMETER, LEDColour
 
 from util.FieldGeometry import (
@@ -19,17 +21,19 @@ class FieldPlayer(BehaviourTask):
     def _initialise_sub_tasks(self):
         self._sub_tasks = {
             "Stand": Stand(self),
+            "P2PPass": P2PPass(self),
         }
 
     def _reset(self):
-        self._current_sub_task = "Stand"
+        self._current_sub_task = "P2PPass"
 
     def _transition(self):
         pass
 
     def _tick(self):
         # Tick sub task!
-        self._tick_sub_task()
+        teammate_pos = get_teammate_pos(1)
+        self._tick_sub_task(pass_target=teammate_pos)
 
 
 
